3aee1c656f5d083df1843d37447de041f227c7d5
Nirni646 <78408320+Nirni646@users.noreply.github.com>
2021-09-14 20:47:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import IssLocationScreen from './screens/IssLocationScreen';
+import MeteorScreen from './screens/MeteorScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <stack.Navigator initialRootName = 'home' screenOptions={{
+        headerShown : false,
+      }}>
+        <stack.Screen name='home' component={HomeScreen} />
+        <stack.Screen name='IssLocation' component={IssLocationScreen} />
+        <stack.Screen name='meteor' component={MeteorScreen} />
+      </stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
